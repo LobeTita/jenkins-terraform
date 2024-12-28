416178bed3fbabd1c460f8b143912fdf33267a29
LobeTita <ajonglobe@gmail.com>
2024-12-28 14:43:12 +0100

    ec2

--- ec2.tf
+++ ec2.tf
@@ -7,14 +7,7 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web1" {
-  ami           = "ami-0453ec754f44f9a4a"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "Test"
-  }
-}
 
 
 # data "aws_ami" "ubuntu" {

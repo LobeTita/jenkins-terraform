56d37439e30edc1bd4d1591bf5a856a4373d7325
LobeTita <ajonglobe@gmail.com>
2024-12-29 04:03:30 +0100

    add instance

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,15 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_instance" "test" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Test"
+  }
+}
+
 
 
 
